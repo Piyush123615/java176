d0d0ddc2359f2df1392bbe2c506599a07f8cc869
Piyush123615 <piyushburde6@gmail.com>
2024-06-24 14:45:27 +0530

    Demo

---
+++ GitDemo/src/com/cjc/main/Demo.java
@@ -0,0 +1,28 @@
+package com.cjc.main;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is Main Method");
+
+	}
+
+	public void M1() {
+		System.out.println("This is m1");
+	}
+	 public void M2()
+	    {
+	    	System.out.println("This is m2");
+	    }
+	public void M3()
+	    {
+	    	System.out.println("This is m3");
+	    }
+	public void M4()
+    {
+    	System.out.println("This is m4");
+    }
+
+
+}
